9aea8b76d29cabb554fc987c595eaf783e1ec25a
11002084 <League@AdministorsiMac.attlocal.net>
2019-02-17 15:57:52 -0800

    Platypus

---
+++ src/Platypus.java
@@ -0,0 +1,11 @@
+
+public class Platypus {
+	      private String name;
+
+	      	void sayHi(){
+	            	System.out.println("The platypus " + name + " is smarter than your average platypus.");
+	      	}
+			Platypus(String platypus){
+				this.platypus=platypus;
+			}
+}
---
+++ src/PlatypusRunner.java
@@ -0,0 +1,8 @@
+
+
+public class PlatypusRunner {
+	public static void main(String[] args) {
+		Platypus platypus=new Platypus("Billy");
+		platypus.sayHi();
+	}
+}
