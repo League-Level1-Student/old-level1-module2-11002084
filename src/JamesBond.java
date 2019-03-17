5c60b112aaf46cbaf1614f152138ff0bdcf1c2b7
varnit05 <League@AdministorsiMac.attlocal.net>
2019-03-17 15:59:16 -0700

    Vault

--- src/JamesBond.java
+++ src/JamesBond.java
@@ -1,7 +1,13 @@
 
 public class JamesBond {
-	JamesBond(int guess){
-		
-	}
+	Vault vault = new Vault(90921);
 
+	int findCode() {
+		for (int i = 0; i < 1000000; i++) {
+			if (vault.tryCode(i)) {
+				return i;
+			}
+		}
+		return -1;
+	}
 }
--- src/Vault.java
+++ src/Vault.java
@@ -1,16 +1,22 @@
 
 public class Vault {
-int code;
-	Vault(int code){
-		this.code=code;
-	}
-	boolean tryCode(int guess){
-		if(guess==code) {
-			return true;
-		}
-		else{
-			return false;
-		}
-	
+	int code;
+
+	Vault(int code) {
+		this.code = code;
 	}
 
+	boolean tryCode(int guess) {
+		if (guess == code) {
+			return true;
+		} else {
+			return false;
+		}
+
+	}
+	public static void main(String[] args) {
+		JamesBond bond=new JamesBond();
+		int answer=bond.findCode();
+		System.out.println(answer);
+	}
+}
